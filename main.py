db7e452a01bcb29081be5b59fd631c8d113b19ad
vaibhavkrishna2314 <vkrishna2314@gmail.com>
2025-04-21 17:42:14 +0530

    error fix

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ logging.basicConfig(filename='app.log', level=logging.INFO)
 def main():
     logging.info("Script started.")
     try:
-        x = 10 / 0  # This will raise ZeroDivisionError
+        x = 10 / 1  # This will raise ZeroDivisionError
     except ZeroDivisionError as e:
         logging.error(f"Error occurred: {e}")
         raise
